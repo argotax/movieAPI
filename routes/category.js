acd67655c287935bbfae9fa7634a4715876f698b
maxime oble <maxoble.dl@gmail.com>
2019-01-18 14:09:53 +0100

    test_error_created_at

--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,59 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url');
+var querystring = require('querystring');
+var models = require('../models');
 
-/* GET home page. */
+/* add a category */
 router.get('/', function(req, res, next) {
+  var params = querystring.parse(url.parse(req.url).query);
+
+  switch (params['type']) {
+    case 'add':
+      models
+      .categories
+      .create({
+        name: params['name']
+      });
+      break;
+
+      
+    case 'delete':
+      models
+      .categories
+      .delete({
+        id: params['id']
+      });
+
+    case 'modify':
+
+      models
+      .categories
+      .findOne({
+        where :{name : params['name']}
+      }).then(
+        category =>{
+          id = category.id;
+          models
+          .categories
+          .update({
+            name : params['name']
+          }),{
+            where : {id : id}
+          }
+        }
+      );
+
+
+
+
+      break;
+    default:
+      console.log('Undefined type');
+  }
+
+
+
   res.render('index', { title: 'Express' });
 });
 
