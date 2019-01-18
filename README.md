f19709e1af584cbf7f60892e9ab2d23663504adb
pierre <mouchelp72531@gmail.com>
2019-01-18 12:03:38 +0100

    'test2'

--- README.md
+++
@@ -1,2 +0,0 @@
-# movieAPI
-api to create a collection of movies with actors and categories itself using api to autocomplete actors' name.
--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url');
+var querystring = require('querystring');
 
 /* GET movie listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var params = querystring.parse(url.parse(req.url).query);
+
+  switch (params['type']) {
+    case 'add':
+      
+      break;
+    case 'Mangoes':
+    case 'Papayas':
+      console.log('Mangoes and papayas are $2.79 a pound.');
+      break;
+    default:
+      console.log('Undefined type');
+  }
+
+
+
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
